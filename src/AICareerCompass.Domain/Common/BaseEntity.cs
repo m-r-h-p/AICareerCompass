1307b2b5d4519e223cf1e2756b3ee46454753ef6
AmirHp <amir.hpid@gmail.com>
2026-07-11 12:26:56 +0330

    Create baseEntity

---
+++ src/AICareerCompass.Domain/Common/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace AICareerCompass.Domain.Common
+{
+    public abstract class BaseEntity<TId>
+    {
+        public TId Id { get; protected set; } = default!;
+
+        private readonly List<object> _domainEvents = new();
+        public IReadOnlyCollection<object> DomainEvents => _domainEvents.AsReadOnly();
+        protected void AddDomainEvent(object domainEvent) => _domainEvents.Add(domainEvent);
+        public void ClearDomainEvents() => _domainEvents.Clear();
+
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not BaseEntity<TId> other) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (GetType() != other.GetType()) return false;
+            return EqualityComparer<TId>.Default.Equals(Id, other.Id);
+        }
+
+        public override int GetHashCode() => Id?.GetHashCode() ?? 0;
+    }
+}
