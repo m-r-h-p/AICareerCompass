7b1052414f05e26ac89a5982471463e5175b9b8d
AmirHp <amir.hpid@gmail.com>
2026-07-11 12:27:31 +0330

    Create domain Exption

---
+++ src/AICareerCompass.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AICareerCompass.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
