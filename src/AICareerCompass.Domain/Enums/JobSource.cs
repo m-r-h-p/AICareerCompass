e71edd65c899cfee6191a26f3aac55423bd4aeb9
AmirHp <amir.hpid@gmail.com>
2026-07-11 12:27:09 +0330

    Add Enumration

---
+++ src/AICareerCompass.Domain/Enums/JobSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICareerCompass.Domain.Enums
+{
+    public enum JobSource
+    {
+        Arbeitnow = 1,
+        Remotive = 2,
+        WeWorkRemotely = 3,
+        Manual = 4
+    }
+    public enum SkillCategory
+    {
+        ProgrammingLanguage = 1,
+        Framework = 2,
+        Database = 3,
+        CloudPlatform = 4,
+        AiMlTool = 5,
+        SoftSkill = 6,
+        Other = 7
+    }
+    public enum ExperienceLevel
+    {
+        Junior = 1,
+        MidLevel = 2,
+        Senior = 3,
+        Lead = 4
+    }
+}
