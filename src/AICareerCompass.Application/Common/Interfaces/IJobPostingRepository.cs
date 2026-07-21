054d0e82afdb73ce3b9fc9da0a8f200a23ee4053
AmirHp <amir.hpid@gmail.com>
2026-07-21 15:25:56 +0330

    Instal MediatR and Create JobPositingRepository

---
+++ src/AICareerCompass.Application/Common/Interfaces/IJobPostingRepository.cs
@@ -0,0 +1,11 @@
+﻿using AICareerCompass.Domain.Entities;
+
+namespace AICareerCompass.Application.Common.Interfaces
+{
+    public interface IJobPostingRepository
+    {
+        Task<JobPosting?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task AddAsync(JobPosting jobPosting, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<JobPosting>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
